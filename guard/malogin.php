6c509d2799af06c6dc2e2f91f41b558ca0d5e0ee
reignya <rhyniaimpas7@gmail.com>
2024-11-18 21:01:35 +0800

    hey

--- guard/malogin.php
+++ guard/malogin.php
@@ -163,6 +163,10 @@ $conn->close();
             color: orange;
             border: solid orange;
         }
+        .navbar{
+                background-color: rgb(53, 97, 255);
+            box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+            }
     @media (max-width: 480px){
     .containers{
         padding-top:10px;
--- guard/malogout.php
+++ guard/malogout.php
@@ -135,7 +135,7 @@ if (isset($_GET['ajax']) && $_GET['ajax'] == 1) {
                 text-align: center;
             }
             .navbar{
-            background-color: rgb(53, 97, 255);
+                background-color: rgb(53, 97, 255);
             box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
             }
             .submit {
