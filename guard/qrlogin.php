b88ead8095bd57ef00283c3e234fda0e31b6e86d
reignya <rhyniaimpas7@gmail.com>
2024-11-19 23:02:16 +0800

    Update qrlogin.php

--- guard/qrlogin.php
+++ guard/qrlogin.php
@@ -1,4 +1,5 @@
 <?php session_start(); 
+date_default_timezone_set('Asia/Manila');
 ini_set('log_errors', 1);
 ini_set('error_log', 'error_log.txt'); // Set log file path
 ini_set('display_errors', 1); // Disable on-screen error display
@@ -163,7 +164,6 @@ $conn->close();
                 </thead>
                 <tbody>
                 <?php
-                date_default_timezone_set('Asia/Manila');
 $server = "localhost";
 $username = "u132092183_parkingz";
 $password = "@Parkingz!2024";
@@ -337,7 +337,7 @@ if (!$query) {
 }
 
 while ($row = $query->fetch_assoc()) {
-    $formattedTimeIn = (new DateTime($row['TIMEIN']))->format('h:i:s A m-d-Y');
+    $formattedTimeIn = (new DateTime($row['TIMEIN']))->format('%Y-%m-%d %H:%i:%s');
     echo "
     <tr>
         <td>" . $row['ID'] . "</td>
