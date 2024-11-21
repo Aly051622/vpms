70dcae0bd247d847d1639c0b6c54414a68a01df9
reignya <rhyniaimpas7@gmail.com>
2024-11-21 21:46:18 +0800

    Update qrlogin.php

--- guard/qrlogin.php
+++ guard/qrlogin.php
@@ -90,10 +90,31 @@ $conn->close();
             text-align: center; 
             margin-top: 10px;
         }
+        .navbar-item .dropbtns:hover{
+            background-color: white;
+            color: orange;
+            border: solid orange;
+            border-radius: 9px;
+        }
+        
+        /*navbar add css*/
         .navbar{
             background-color: rgb(53, 97, 255);
             box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
             }
+        @media (max-width: 480px){
+        .container{
+            padding-top:10px;
+            margin-top:-8px;
+        }
+        .navbar-brand{
+            margin-left: 10px;
+        }
+        .navbar-toggler{
+            margin-top: -33px;
+            margin-left: 11em;
+        }
+    }
     </style>
 </head>
 <body>
