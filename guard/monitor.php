e5396a19598fc921fc54d320ac9152ea71efbdc8
reignya <rhyniaimpas7@gmail.com>
2024-11-22 13:15:49 +0800

    Update monitor.php

--- guard/monitor.php
+++ guard/monitor.php
@@ -1,14 +1,4 @@
 <?php
-
-// Start the session
-session_start();
-
-// Check if the user is logged in, if not redirect to login page
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: qrlogin.php");
-    exit;
-}
-
 // Database connection
 $server = "localhost";
 $username = "u132092183_parkingz";
