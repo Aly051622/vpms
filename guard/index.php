0416216d912e56e531163ad5881daf8060874c6d
reignya <rhyniaimpas7@gmail.com>
2024-11-22 13:26:14 +0800

    Update index.php

--- guard/index.php
+++ guard/index.php
@@ -3,11 +3,6 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Prevent caching
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
 // Include the database connection
 include('../DBconnection/dbconnection.php');
 
