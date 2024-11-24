64f9257c8479b6cf7931986321241aae9a59c96d
reignya <rhyniaimpas7@gmail.com>
2024-11-24 21:49:02 +0800

    Update malogin.php

--- guard/malogin.php
+++ guard/malogin.php
@@ -468,7 +468,7 @@ $conn->close();
                         <td><?= htmlspecialchars($vehicle['RegistrationNumber']); ?></td>
                         <td id="slot_<?= htmlspecialchars($vehicle['id']); ?>"><?= htmlspecialchars($vehicle['ParkingSlot']); ?></td>
 
-                        <td><?= date("h:i:s A m-d-y", strtotime($vehicle['TimeIn'])); ?></td>
+                        <td><?= (new DateTime($vehicle['TimeIn']))->format('h:i A m-d-Y'); ?></td>
                         <td>
                             <button class="btn btn-warning btn-sm" onclick="editSlot(<?= $vehicle['id'] ?>)" id="editbtn">Edit</button>
                             <button class="btn btn-danger btn-sm" onclick="deleteVehicle(<?= $vehicle['id'] ?>)" id="deletebtn">Delete</button>
