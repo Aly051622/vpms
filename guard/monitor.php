03a8878e83a0fc2e0f31f15c4e76b5a318a4d841
reignya <rhyniaimpas7@gmail.com>
2024-11-24 20:47:24 +0800

    Update monitor.php

--- guard/monitor.php
+++ guard/monitor.php
@@ -520,10 +520,10 @@ h4{
         <form method="POST" action="monitor.php">
             <div class="add-slot">
                 <select name="area" id="areaSelect"> 
-                    <option value="Front Admin" selected>Front Admin</option>
-                    <option value="Beside CME">Beside CME</option>
-                    <option value="Kadasig">Kadasig</option>
-                    <option value="Behind">Behind</option>
+                    <option value="Front Admin" selected>A</option>
+                    <option value="Beside CME">B</option>
+                    <option value="Kadasig">C</option>
+                    <option value="Behind">D</option>
                 </select>
                 <select name="status" id="areaSelect">
                     <option value="Vacant">Vacant</option>
@@ -536,10 +536,10 @@ h4{
 
         <!-- Select Area -->
         <div class="select-area">
-    <button id="btnFrontAdmin" onclick="selectArea('Front Admin')">Front Admin</button>
-    <button id="btnBesideCME" onclick="selectArea('Beside CME')">Beside CME</button>
-    <button id="btnKadasig" onclick="selectArea('Kadasig')">Kadasig</button>
-    <button id="btnBehind" onclick="selectArea('Behind')">Behind</button>
+    <button id="btnFrontAdmin" onclick="selectArea('Front Admin')">A</button>
+    <button id="btnBesideCME" onclick="selectArea('Beside CME')">B</button>
+    <button id="btnKadasig" onclick="selectArea('Kadasig')">C</button>
+    <button id="btnBehind" onclick="selectArea('Behind')">D</button>
 </div>
 
 </div>
