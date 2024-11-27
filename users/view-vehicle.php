8b4bd283a8241a829878152888f7a2f320da4ee7
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-27 11:19:39 +0800

    Update view-vehicle.php

--- users/view-vehicle.php
+++ users/view-vehicle.php
@@ -2,7 +2,7 @@
 session_start();
 error_reporting(0);
 include('../DBconnection/dbconnection.php');
-if (strlen($_SESSION['vpmsuid']==0)) {
+if (strlen($_SESSION['vpmsaid']==0)) {
   header('location:logout.php');
   } else{
 
