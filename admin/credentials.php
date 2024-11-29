3dc90889665f20be86c873e294232577852d5623
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-29 23:00:33 +0800

    Update credentials.php

--- admin/credentials.php
+++ admin/credentials.php
@@ -38,10 +38,10 @@ if ($resultAllUsers && mysqli_num_rows($resultAllUsers) > 0) {
                 <td>{$row['LastName']}</td>
                 <td>{$row['Email']}</td>
                 <td>{$row['MobileNumber']}</td>
-                <td><img src='../uploads/{$row['or_image']}' alt='OR Image' width='100'></td>
-                <td><img src='../uploads/{$row['cr_image']}' alt='CR Image' width='100'></td>
-                <td><img src='../uploads/{$row['nv_image']}' alt='NV Image' width='100'></td>
-                <td><img src='../uploads/{$row['profile_pictures']}' alt='Profile Picture' width='100'></td>
+                <td><img src='uploads/{$row['or_image']}' alt='OR Image' width='100'></td>
+                <td><img src='uploads/{$row['cr_image']}' alt='CR Image' width='100'></td>
+                <td><img src='uploads/{$row['nv_image']}' alt='NV Image' width='100'></td>
+                <td><img src='uploads/{$row['profile_pictures']}' alt='Profile Picture' width='100'></td>
               </tr>";
     }
 
