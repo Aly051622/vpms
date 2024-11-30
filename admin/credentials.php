a3eb807026572debb3403cdcdbb05b853bca4354
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-30 08:21:15 +0800

    Update credentials.php

--- admin/credentials.php
+++ admin/credentials.php
@@ -38,17 +38,17 @@ include 'users_view.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="apple-touch-icon" href="images/ctu.png">
     <link rel="shortcut icon" href="images/ctu.png">
-    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>All Users</title>
     <style>
@@ -125,44 +125,34 @@ include 'users_view.php';
                     <td><?= htmlspecialchars($user['Email']) ?></td>
                     <td><?= htmlspecialchars($user['MobileNumber']) ?></td>
                     <td>
-    <?php if (!empty($user['or_image']) && file_exists('uploads/' . $user['or_image'])): ?>
-        <img src="uploads/<?= htmlspecialchars(['or_image']) ?>" alt="OR Image" style="max-width: 100px; max-height: 100px;">
-    <?php else: ?>
-        N/A
-    <?php endif; ?>
-</td>
-<td>
-    <?php if (!empty($user['or_image']) && file_exists('uploads/' . $user['or_image'])): ?>
-        <img src="uploads/<?= htmlspecialchars($user['or_image']) ?>" alt="OR Image" style="max-width: 100px; max-height: 100px;">
-    <?php else: ?>
-        N/A
-    <?php endif; ?>
-</td>
-
-<td>
-    <?php if (!empty($user['cr_image']) && file_exists('uploads/' . $user['cr_image'])): ?>
-        <img src="uploads/<?= htmlspecialchars($user['cr_image']) ?>" alt="CR Image" style="max-width: 100px; max-height: 100px;">
-    <?php else: ?>
-        N/A
-    <?php endif; ?>
-</td>
-
-<td>
-    <?php if (!empty($user['nv_image']) && file_exists('uploads/' . $user['nv_image'])): ?>
-        <img src="uploads/<?= htmlspecialchars($user['nv_image']) ?>" alt="NV Image" style="max-width: 100px; max-height: 100px;">
-    <?php else: ?>
-        N/A
-    <?php endif; ?>
-</td>
-
-<td>
-    <?php if (!empty($user['profile_pictures']) && file_exists('../../uploads/' . $user['profile_pictures'])): ?>
-        <img src="../../uploads/<?= htmlspecialchars($user['profile_pictures']) ?>" alt="Profile Picture" style="max-width: 100px; max-height: 100px;">
-    <?php else: ?>
-        N/A
-    <?php endif; ?>
-</td>
-
+                        <?php if (!empty($user['or_image'])): ?>
+                            <img src="../uploads/<?= htmlspecialchars($user['or_image']) ?>" alt="OR Image">
+                        <?php else: ?>
+                            N/A
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php if (!empty($user['cr_image'])): ?>
+                            <img src="../uploads/<?= htmlspecialchars($user['cr_image']) ?>" alt="CR Image">
+                        <?php else: ?>
+                            N/A
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php if (!empty($user['nv_image'])): ?>
+                            <img src="../uploads/<?= htmlspecialchars($user['nv_image']) ?>" alt="NV Image">
+                        <?php else: ?>
+                            N/A
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php if (!empty($user['profile_pictures'])): ?>
+                            <img src="../uploads/<?= htmlspecialchars($user['profile_pictures']) ?>" alt="Profile Picture">
+                        <?php else: ?>
+                            N/A
+                        <?php endif; ?>
+                    </td>
+                </tr>
             <?php endforeach; ?>
         </table>
                         </div>
@@ -174,6 +164,7 @@ include 'users_view.php';
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 <script src="assets/js/main.js"></script>
 </body>
 </html>
