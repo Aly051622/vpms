84c813cff139d0f78f61f9f67479790f42393b5c
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-01 22:57:56 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -350,14 +350,15 @@ if (isset($_POST['submit'])) {
                                 <li><i class="menu-icon fa fa-address-book"></i><a href="reg-users.php">User Information</a></li>
                                 <li><i class="menu-icon  fa bi bi-chat-dots-fill"></i><a href="admin_comments.php">Comment</a></li>
                                 <li><i class="menu-icon fa  bi bi-envelope-paper-heart"></i><a href="admin_feedbacks.php">Feedback</a></li>
-                                <li><i class="menu-icon fa  bi bi-headset"></i><a href="admin_service.php">Customer Service</a></li>
+                              <!--  <li><i class="menu-icon fa  bi bi-headset"></i><a href="admin_service.php">Customer Service</a></li>-->
                             </ul>
                     </li>
                     <li>
+                        <!--
                         <a href="#" onclick="showPasswordModal('credentials', 'credentials.php')">
                             <i class="menu-icon fa bi bi-geo-fill"></i>Credentials
                         </a>
-                    </li>
+                    </li>-->
                 </ul>
             </div><!-- /.navbar-collapse -->
         </nav>
