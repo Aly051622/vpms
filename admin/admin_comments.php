44d44b4d4db4e5800f6266be8542a8784fec7470
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-01 19:23:05 +0800

    Update admin_comments.php

--- admin/admin_comments.php
+++ admin/admin_comments.php
@@ -519,7 +519,7 @@ if ($result && mysqli_num_rows($result) > 0) {
         <?php foreach ($comments as $comment): ?>
             <?php 
                 // Format the created_at date
-                $formatted_date = date("h:i A\nm / d / Y", strtotime($comment['created_at']));
+                $formatted_date = date("h:i A\nm/d/Y", strtotime($comment['created_at']));
             ?>
             <tr>
                 <td><?= htmlspecialchars($comment['username']) ?></td>
