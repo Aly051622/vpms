1f5db59e264fd8c697236fe1f936ca440e09916f
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-01 21:17:11 +0800

    Update invalidated.php

--- admin/invalidated.php
+++ admin/invalidated.php
@@ -7,10 +7,7 @@ $queryInvalidated = "
     SELECT u.email, 
            MAX(u.expiration_date) AS expiration_date, 
            MAX(u.validity) AS validity, 
-           MAX(r.cr_image) AS cr_image, 
-           MAX(r.nv_image) AS nv_image, 
-           MAX(r.or_image) AS or_image, 
-           MAX(r.profile_pictures) AS profile_pictures 
+         
     FROM uploads u
     JOIN tblregusers r ON u.email = r.Email
     WHERE u.validity = 0
@@ -132,10 +129,7 @@ mysqli_close($con);
                         <th>Email</th>
                         <th>Expiration Date</th>
                         <th>Validity</th>
-                        <th>CR Image</th>
-                        <th>NV Image</th>
-                        <th>OR Image</th>
-                        <th>Profile Picture</th>
+                       
                     </tr>
                 </thead>
                 <tbody>
