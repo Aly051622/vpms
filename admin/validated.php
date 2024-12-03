8cbfa213699bc8a882f14e6942940a25b129fcde
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-03 22:52:20 +0800

    Update validated.php

--- admin/validated.php
+++ admin/validated.php
@@ -128,6 +128,7 @@ mysqli_close($con);
         </table>
     </div>
 </div>
+
 <!-- Scripts -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
