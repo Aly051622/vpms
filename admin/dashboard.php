b7c685d1a4d1f0fe0cb751fd913a31d130f33ef3
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 07:09:02 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -271,6 +271,50 @@ $count_total_vehexits=mysqli_num_rows($query2);
 
 <div class="col-lg-3 col-md-6">
     <?php
+    // Total Registered Vehicles
+    $queryVehicles = mysqli_query($con, "
+        SELECT COUNT(DISTINCT email) AS vehicleCount 
+        FROM tblvehicle
+    ");
+    $vehicleCount = mysqli_fetch_assoc($queryVehicles)['vehicleCount'];
+    ?>
+    <div class="card">
+        <div class="card-body">
+            <div class="stat-widget-five">
+                <div class="stat-icon dib flat-color-1">
+                    <i class="pe-7s-car"></i>
+                </div>
+                <div class="stat-content">
+                    <div class="text-left dib">
+                        <div class="stat-text"><span class="count"><?php echo $vehicleCount; ?></span></div>
+                        <div class="stat-heading">Total Registered Vehicles</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-lg-3 col-md-6">
+    <?php
     // Total Vehicle Categories
     $query = mysqli_query($con, "SELECT ID FROM tblcategory");
     $listedcat = mysqli_num_rows($query);
