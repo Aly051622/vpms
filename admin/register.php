ea5075faaa44529bdcaa8be62b389fea39bc0ec0
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 10:10:38 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -106,7 +106,7 @@ if (isset($_POST['submit'])) {
       overflow: hidden;
     }
     .content{
-        margin-top: 20em;
+        margin-top: -20em;
         margin-left: -5em;
     }
     #page1 {
