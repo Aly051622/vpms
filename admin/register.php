fe4515c0570e6ba01df630d6d8642dd98e841c20
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 11:21:09 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -548,10 +548,21 @@ input[type="text"]:hover, input[type="password"]:hover {
                                 <span class="fa bi bi-person-fill" style="font-size: 20px"></span>
                                 <input type="email" name="email" placeholder="Email address" required class="form-control">
                             </div>
-                            <div class="form-group field space">
-                                <span class="fa bi bi-lock-fill" style="font-size: 20px"></span>
-                                <input type="password" name="password" id="password" placeholder="Enter password" required class="form-control">
-                            </div>
+                         
+<div class="row form-group">
+    <div class="col col-md-3">
+        <label for="newpassword" class="form-control-label">New Password</label>
+    </div>
+    <div class="col-12 col-md-9 position-relative">
+        <input 
+            type="password" 
+            name="newpassword" 
+            id="newpassword" 
+            class="form-control" 
+            required="true" 
+            pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" 
+            title="Password must be at least 8 characters long, include uppercase and lowercase letters, a number, and a special character."
+        >
                             <div class="form-group field space">
                                 <span class="fa bi bi-shield-lock-fill" style="font-size: 20px"></span>
                                 <input type="password" name="repeatpassword" id="repeatpassword" placeholder="Repeat password" required class="form-control">
