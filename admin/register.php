a1daafadd09738752796b9bd1a054e1a6a052f0e
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 09:55:36 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -400,8 +400,8 @@ input[type="text"]:hover, input[type="password"]:hover {
 <div id="right-panel" class="right-panel" >
 <header id="header" class="header" >
             <div class="top-left">
-                <div class="navbar-header" style="background-image: linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%);" style="margin-top: -10px;">
-                    <a class="navbar-brand" href="dashboard.php"><img src="images/hylogo.png" alt="Logo" style=" width: 120px; height: auto;"></a>
+                <div class="navbar-header" style="background-image: linear-gradient(to top, #1e3c72 0%, #1e3c72 1%, #2a5298 100%);" >
+                    <a class="navbar-brand" href="dashboard.php"><img src="images/hylogo.png" alt="Logo" style=" width: 120px; height: auto; margin-top:-10px;"></a>
                     <a class="navbar-brand hidden" href="./"><img src="images/logo3.png" alt="Logo"></a>
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
