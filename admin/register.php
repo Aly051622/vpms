2567992fc1a20ea17d7e6b88b285cfb51db55cec
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 10:20:45 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -88,8 +88,7 @@ if (isset($_POST['submit'])) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
       <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
       <link rel="stylesheet" href="assets/css/style.css">
-  
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
        
 
